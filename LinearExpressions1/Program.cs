7d73f8f829516b56c0b741f2442b72d15e57a95f
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2023-11-07 14:15:50 +0200

    Sana01

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введіть число а: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число b: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число c: ");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число d: ");
+double d = double.Parse(Console.ReadLine());
+double x, y, z, r;
+x = (a + 2 * b - c + d) / (c * d) + (a+b)/(c-d) - (a*a)/(b*b);
+y = (5 * (a + b) * (c - d)) / ((1.0 / 2) * c) + d * d * (( a * a - b * b)/( b - a));
+z = (Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) / (1 - b);
+r = ((1.0 / 2) * a + (3.0 / 4) * b - 7.0 / 5) / (3 * c + 1) + 1 / (a - c);
+Console.WriteLine("x = " + x);
+Console.WriteLine("y = " + y);
+Console.WriteLine("z = " + z);
+Console.WriteLine("r = " + r);
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введіть число m: ");
+double m = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число n: ");
+double n = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число b: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число a: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число x: ");
+double x = double.Parse(Console.ReadLine());
+double z1, z2, y;
+z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n)) + n * m + m * m - m;
+Console.WriteLine("z1 = " + z1);
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine("z2 = " + z2);
+y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+Console.WriteLine("y = " + y);
